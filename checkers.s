9ed6cdc683ddb975f6cf817261764db25cd37492
Vitor Pontes <vitormax2005@hotmail.com>
2018-07-09 05:52:00 +0000

    Clear options for victory and defeat

--- checkers.s
+++ checkers.s
@@ -146,9 +146,11 @@ enemy_options: .word 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
 	j turn_start
 
 	defeat:
+	jal print_step
 	print_string(defeat_text)
 	j exit
 	victory:
+	jal print_step
 	print_string(victory_text)
 	exit:
 	j exit
